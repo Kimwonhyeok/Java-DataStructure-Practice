d3b97ba46c73e10afa62317025e093485ef8546c
Weon_hyeok2 <kwh102455@gmail.com>
2025-04-01 21:12:59 +0900

    Update StackInterface.java

--- stack/StackInterface.java
+++ stack/StackInterface.java
@@ -2,13 +2,22 @@ package stack;
 
 public interface StackInterface {
     public void push(int value);
+
     public void pop();
+
     public void peek();
-    public void indexOf();
+
+    public int indexOf(int value);
+
     public void clear();
+
     public void capacity();
+
     public void size();
+
     public boolean isEmpty();
+
     public boolean isFull();
+
     public void dump();
 }
