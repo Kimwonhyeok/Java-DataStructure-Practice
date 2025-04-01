dbe5f3dc1d3347d31c3fb364fbea00d57626008c
Weon_hyeok2 <kwh102455@gmail.com>
2025-04-01 20:24:16 +0900

    Create StackInterface.java

---
+++ stack/StackInterface.java
@@ -0,0 +1,14 @@
+package stack;
+
+public interface StackInterface {
+    public void push(int value);
+    public void pop();
+    public void peek();
+    public void indexOf();
+    public void clear();
+    public void capacity();
+    public void size();
+    public boolean isEmpty();
+    public boolean isFull();
+    public void dump();
+}
