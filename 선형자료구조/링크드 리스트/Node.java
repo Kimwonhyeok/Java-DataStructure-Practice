3d6067c630c5bed9865a93ad67b029cfa619a1a1
김원혁 <kwh102455@gmail.com>
2025-05-11 21:42:36 +0900

    feat: first commit - linked list  빌드 해보기 - SinglyLinkedList

---
+++ 선형자료구조/링크드 리스트/Node.java
@@ -0,0 +1,17 @@
+package linkedlist;
+
+/**
+ * 단순 연결 리스트
+ * @instance variable
+ * data = 데이터 값 저장 변수
+ * link = 다음 노드 참조값 저장 변수
+ */
+public class Node {
+    Object data;
+    Node link;
+
+    Node(Object data){
+        this.data = data;
+        this.link = null;
+    }
+}
