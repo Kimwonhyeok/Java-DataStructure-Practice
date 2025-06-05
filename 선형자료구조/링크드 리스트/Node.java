c3588c45b80513de843100a6481382ee29a91a2f
Weon_hyeok2 <kwh102455@gmail.com>
2025-06-05 14:57:38 +0900

    Update Node.java

--- 선형자료구조/링크드 리스트/Node.java
+++ 선형자료구조/링크드 리스트/Node.java
@@ -1,17 +1,24 @@
 package linkedlist;
 
-/**
- * 단순 연결 리스트
- * @instance variable
- * data = 데이터 값 저장 변수
- * link = 다음 노드 참조값 저장 변수
- */
-public class Node {
-    Object data;
-    Node link;
+public class Node<E> {
+    private E data;
+    public Node next;
 
-    Node(Object data){
+    Node() {
+        data = null;
+        next = null;
+    }
+
+    Node(E data) {
         this.data = data;
-        this.link = null;
+        next = null;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public E getData() {
+        return data;
     }
 }
